dbbb8b8102bb6ba596feb41938cd6dded0364705
vtrtga <vitor.ypvalim@gmail.com>
2025-07-11 22:04:36 -0400

    test: user test

---
+++ backend/src/__tests__/user/user.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import app from '../../index';
+import prisma from '../../prisma/client';
+
+describe('User routes', () => {
+  beforeAll(async () => {
+    await prisma.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  it('deve criar um novo usuário', async () => {
+    const res = await request(app).post('/user').send({
+      name: 'Vitor',
+      email: 'vitor@test.com',
+      password: '123456',
+      phone: '65999999999',
+      birthDate: '1998-10-10',
+    });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty('id');
+    expect(res.body.email).toBe('vitor@test.com');
+  });
+
+  it('deve retornar lista de usuários', async () => {
+    const res = await request(app).get('/user');
+
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
+});
